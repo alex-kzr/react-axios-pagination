ba0cbbed6c6245c08fa913a73a78493c0dfd05db
Alex-SK <aleksey.kozyura@gmail.com>
2020-05-25 04:34:32 +1000

    Add Posts component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import Posts from './components/Posts';
 import axios from 'axios';
 import './App.css';
 
-function App() {
+
+const App = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPate, setPostsPerPage] = useState(10);
+  const [postsPerPage] = useState(10);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -14,14 +16,16 @@ function App() {
       const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
       setPosts(res.data);
       setLoading(false);
-    }
+    };
 
     fetchPosts();
   }, []);
 
   return (
-    <div className="App">
-      React App
+    <div className='container mt-5'>
+      <h1 className='text-primary mb-3'>My Blog</h1>
+      <Posts posts={posts} loading={loading} />
+      
     </div>
   );
 }
