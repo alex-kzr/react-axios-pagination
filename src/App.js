d6ae74c8caceb31b30948ecb4210daa7b0686725
Alex-SK <aleksey.kozyura@gmail.com>
2020-06-07 22:07:45 +1000

    Add current page

--- src/App.js
+++ src/App.js
@@ -27,11 +27,13 @@ const App = () => {
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost); 
 
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
   return (
     <div className='container mt-5'>
       <h1 className='text-primary mb-3'>My Blog</h1>
       <Posts posts={currentPosts} loading={loading} />
-      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} />
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate} />
     </div>
   );
 }
